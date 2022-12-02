8a5996c46985dc1275afb58aa7ab635a9a184891
Santiago Weihmüller <61631736+SantiW01@users.noreply.github.com>
2022-12-02 20:06:37 -0300

    Add Routes for its components and style Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Style/App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import LogIn from "./Components/LogIn";
+import SignIn from "./Components/SignIn";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route></Route>
-        <Route></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<LogIn />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,28 @@
 import React from "react";
 import "../Style/Header.css";
+import { Navbar, Container, ButtonGroup, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export default function Headert() {
-  return <div>Headert</div>;
+export default function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <Link to="/">Home</Link>
+        </Navbar.Brand>
+        <ButtonGroup>
+          <Button>
+            <Link to="logIn" style={{ color: "white" }}>
+              Log In
+            </Link>
+          </Button>
+          <Button>
+            <Link to="signIn" style={{ color: "white" }}>
+              Sign In
+            </Link>
+          </Button>
+        </ButtonGroup>
+      </Container>
+    </Navbar>
+  );
 }
