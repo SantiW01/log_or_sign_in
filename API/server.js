ac6cd5d041aa1bb9e6d8fd31a6ff801d8363b732
Santiago Weihmüller <61631736+SantiW01@users.noreply.github.com>
2022-12-14 21:51:59 -0300

    Add server

--- API/server.js
+++ API/server.js
@@ -1,2 +1,21 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to DataBase"));
+
+app.use(express.json());
+
+const insertUser = require("./Routers/InsertSubscribers");
+app.use("/subscribers", insertUser);
+
+app.listen(8080, () => {
+  console.log("Server listening at 8080");
+});
