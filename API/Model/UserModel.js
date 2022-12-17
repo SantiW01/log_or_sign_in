a7712f4bf368bc8cf23dfe94d6ad762a879183ce
Santiago Weihmüller <61631736+SantiW01@users.noreply.github.com>
2022-12-16 23:45:21 -0300

    Add a Hash to the password

--- API/Model/UserModel.js
+++ API/Model/UserModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const userModel = mongoose.Schema({
   user_name: {
     type: String,
@@ -13,6 +13,17 @@ const userModel = mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userModel.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(this.user_password, salt);
+    this.user_password = hashPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = mongoose.model("user", userModel);
